bf9de567a86081924e145de17fd034e5d2dd783b
Mateus Mabolacha <mabolacha58@gmail.com>
2026-04-29 09:44:27 +0200

    fix: main.tsx e routes.tsx usar react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router'
+import { RouterProvider } from 'react-router-dom'
 import { AuthProvider } from './app/context/AuthContext'
 import { router } from './app/routes'
 import './styles/index.css'
